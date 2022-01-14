74f014f44b6797170f41b7e9255ea6ad13c7b596
GonyaDev <gonyadev@yandex.ru>
2022-01-14 17:04:38 +0300

    Initial Commit

---
+++ CreditCounter/CreditCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCounterApp.swift
+//  CreditCounter
+//
+//  Created by Алексей Гончаров on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CreditCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
